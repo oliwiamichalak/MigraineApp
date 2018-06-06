9661f068a81f3395bb1a520b9fab692eb68b028e
Oliwia Michalak <oliwia.michalak@gmail.com>
2018-06-06 10:10:06 +0200

    Initial commit

---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MigraineApp
+//
+//  Created by Oliwia Michalak on 06.06.2018.
+//  Copyright © 2018 Oliwia Michalak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
